3e4c844668ee15aa318d92c63ab48e2111824fca
Gabriel Calvo Vargas <gvargas@sparkcognition.com>
2021-05-29 10:36:11 -0600

    pruea

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello colegas de Proyecto 3!";
     }
 
     @RequestMapping("/*")
